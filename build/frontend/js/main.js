9cf1e25af3576eb6cfd0a7c8492c767274273291
Irina403 <irinat403@gmail.com>
2022-05-12 10:58:56 +0300

    123

--- build/frontend/js/main.js
+++ build/frontend/js/main.js
@@ -110,3 +110,25 @@ $( "#mobile-nav__item" ).click(function(e) {
       $(this).fadeOut();					
     }
 });
+
+// tabs
+function tabs(link, block) {
+  let linkSelector = $(link),
+      blockSelector = $(block);
+
+  $(linkSelector).on('click', function (e) {
+      e.preventDefault();
+
+      let $this = $(this),
+          currentData = $(this).data('tab');
+
+      $(blockSelector).removeClass('active_tab');
+      $(linkSelector).removeClass('active_tab');
+
+      $(block + '[data-tab="' + currentData + '"]').addClass('active_tab');
+      $this.addClass('active_tab');
+
+  });
+}
+
+tabs('.professionals__btn', '.doc_itemProff');
